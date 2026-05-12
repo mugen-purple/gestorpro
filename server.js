f1a4532fa23730e3beafe80524e4b0b4106be8d6
Davi Andrade <davi.andrade.210907@gmail.com>
2026-05-12 01:28:43 -0300

    Configurando porta do Render

--- server.js
+++ server.js
@@ -569,8 +569,10 @@ app.post('/bloquear-usuario/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log('Servidor rodando em http://localhost:3000!');
+app.listen(PORT, () => {
+
+    console.log(`Servidor rodando na porta ${PORT}`);
 
 });
